ad98e1c188e30d7ea3a9fa98ff45d214b2c89947
magnusfra <magnusfrantsen@gmail.com>
2026-09-20 14:33:26 +0200

    test på endring i app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ st.title("Min første Streamlit-app 🚀")
 st.write("Hei! Hvis du ser dette, fungerer alt som det skal.")
 
 # Enkel interaktiv komponent for å sjekke at alt reagerer
-navn = st.text_input("Hva heter du?")
+navn = st.text_input("Hva heter du egentlig da?")
 if navn:
     st.success(f"Velkommen til appen, {navn}!")
 
