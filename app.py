8c3b037b326e7de3699e16e5318eb3a9a21f378e
MagnusFrantsen <magnus.frantsen@nmbu.no>
2026-09-21 15:09:55 +0200

    Made changes to the structure of home page

--- app.py
+++ app.py
@@ -1,8 +1,17 @@
 import streamlit as st
 
 def main():
-    st.title("Front page")
-    st.write("This is the front page of the app.")
+    st.title("Home Page")
+    st.write("This is the home page of the app. You can navigate to the analytics page using the sidebar menu.")
+
+    # Enkel interaktiv komponent for å sjekke at alt reagerer
+    wish = st.text_input("What do you want to do?")
+    if wish:
+        st.success(f"You want to do: {wish}")
+
+    place = st.text_input("Where are you?")
+    if place:
+        st.success(f"You are in {place}!")
 
 def analytics_page():
     st.title("Analytics page")
@@ -10,24 +19,12 @@ def analytics_page():
 
 pages = {
     "Home": [
-        st.Page(main,title ="Front page"),
+        st.Page(main,title ="Home Page"),
     ],
     "Analytics": [
         st.Page(analytics_page, title="Analytics")
     ],
 }
-
-st.title("This is the front page of the app.")
-st.write("This is the front page of the app. You can navigate to the analytics page using the sidebar menu.")
-
-# Enkel interaktiv komponent for å sjekke at alt reagerer
-wish = st.text_input("What do you want to do?")
-if wish:
-    st.success(f"You want to do: {wish}")
-
-place = st.text_input("Where are you?")
-if place:
-    st.success(f"You are in {place}!")
 
 pg = st.navigation(pages)
 pg.run()
